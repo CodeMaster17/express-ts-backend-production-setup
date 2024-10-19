59b75d9bc8ae77dca3982f53478af7aee3a31a4f
Harshit Yadav <2105891@kiit.ac.in>
2024-10-19 20:40:50 +0530

    feat: File logger setup

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,6 +1,9 @@
-import { createLogger, format, transports } from 'winston';
-import { ConsoleTransportInstance } from 'winston/lib/winston/transports';
+import path from 'path';
 import util from 'util';
+import { createLogger, format, transports } from 'winston';
+import { ConsoleTransportInstance, FileTransportInstance } from 'winston/lib/winston/transports';
+import config from '../config/config';
+import { EApplicationEnviroment } from '../constants/application';
 
 const consoleLogFormat = format.printf((info) => {
     const { level, message, timestamp, meta = {} } = info;
@@ -19,10 +22,50 @@ const consoleLogFormat = format.printf((info) => {
 });
 
 const consoleTransport = (): Array<ConsoleTransportInstance> => {
+    // printing in console only if environment is development
+    if (config.ENV === EApplicationEnviroment.DEVELOPMENT) {
+        return [
+            new transports.Console({
+                level: 'info',
+                format: format.combine(format.timestamp(), consoleLogFormat)
+            })
+        ];
+    }
+    return [];
+};
+
+const FileLogFormat = format.printf((info) => {
+    const { level, message, timestamp, meta = {} } = info;
+
+    const logMeta: Record<string, unknown> = {};
+    for (const [key, value] of Object.entries(meta)) {
+        if (value instanceof Error) {
+            logMeta[key] = {
+                name: value.name,
+                message: value.message,
+                trace: value.stack || ''
+            };
+        } else {
+            logMeta[key] = value;
+        }
+    }
+
+    const logData = {
+        level: level.toUpperCase(),
+        message,
+        timestamp,
+        meta: logMeta
+    };
+    return JSON.stringify(logData, null, 4);
+});
+
+// for file transport
+const FileTransport = (): Array<FileTransportInstance> => {
     return [
-        new transports.Console({
+        new transports.File({
+            filename: path.join(__dirname, '../', '../', 'logs', `${config.ENV}.log`),
             level: 'info',
-            format: format.combine(format.timestamp(), consoleLogFormat)
+            format: format.combine(format.timestamp(), FileLogFormat)
         })
     ];
 };
@@ -31,6 +74,6 @@ export default createLogger({
     defaultMeta: {
         meta: {}
     },
-    transports: [...consoleTransport()]
+    transports: [...consoleTransport(), ...FileTransport()]
 });
 
