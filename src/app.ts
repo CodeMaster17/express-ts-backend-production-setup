8f7eae278a33e57efb71f73da16a95f209512af3
Harshit Yadav <2105891@kiit.ac.in>
2024-11-03 12:47:50 +0530

    feat: Added helmet middleware

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,12 @@ import router from './router/apiRouter';
 import globalErrorHandler from './middleware/globalErrorHandler';
 import { responseMessage } from './constants/responseMessage';
 import httpError from './utils/httpError';
+import helmet from 'helmet';
 
 const app: Application = express();
 
 // middleware
+app.use(helmet());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../', 'public')));
 
